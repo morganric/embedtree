da03502c608a5f1df97ae065279543fd64b82e12
morganric <morganric@gmail.com>
2015-09-07 15:30:23 +0100

    this

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,8 @@ belongs_to :user
 validates_presence_of :url
 validates :url, :format => URI::regexp(%w(http https))
 
-validates :provider, :inclusion => {:in => %w("Mixcloud", "SoundCloud", "Vimeo", "YouTube"), 
-		:message => "not a provider" }
+validates_inclusion_of :provider, :in => ["Mixcloud", "SoundCloud", "Vimeo", "YouTube"], 
+		:message => "not a provider" 
 
 extend FriendlyId
  friendly_id :title, use: :slugged
