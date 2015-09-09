afdc5d8a90910b0d8084ca9c19cd7f12b4f1ba32
morganric <morganric@gmail.com>
2015-09-09 18:25:24 +0100

    feed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all.order('created_at DESC').page params[:page]
+    @posts = Post.all.order('views DESC').page params[:page]
   end
 
   def author
